959750bb8564e6f75769ce94483a1e31d382f7e2
Judson Mello do Bomfim <falecom@judsonbomfim.com>
2023-08-25 11:57:09 -0300

    Update frete

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -215,7 +215,9 @@ def ord_import(request):
                             if i['key'] == 'Data de Ativação': activation_date_i = dateF(i['value'])
                             if i['key'] == 'Modelo e marca de celular': cell_mod_i = i['value']
                             if i['key'] == 'Número de pedido ou do chip': ord_chip_nun_i = i['value']
-                        shipping_i = order['shipping_lines'][0]['method_title'] or ''
+                        if order['shipping_lines'][0]['method_title']:
+                            shipping_i = order['shipping_lines'][0]['method_title']
+                        else: shipping_i = '-'
                         order_date_i = dateHour(order['date_created'])
                         # notes_i = 0
                         
