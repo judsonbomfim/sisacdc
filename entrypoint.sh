c6fae58e06367a7172c33a1d27500b332760c218
Judson Bomfim <falecom@judsonbomfim.com>
2024-01-11 20:43:04 -0300

    collectstatic

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 python manage.py makemigrations
 python manage.py migrate
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 2 --threads 2 --log-level=info & celery -A core worker -B --loglevel=info
