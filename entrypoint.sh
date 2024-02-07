edfc08653e57fdf26a83de470dc85003e4003017
Judson Bomfim <falecom@judsonbomfim.com>
2024-02-07 18:47:54 -0300

    Correção de permissões

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn core.wsgi:application --bind 0.0.0.0:8000 --log-level=info & celery -A core worker -B --loglevel=info
