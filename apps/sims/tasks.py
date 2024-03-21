c04bd6e36a1b654033ab3727fcffd0b9717db7ee
Judson Bomfim <falecom@judsonbomfim.com>
2024-03-21 13:41:37 -0300

    Ajuste telcom

--- apps/sims/tasks.py
+++ apps/sims/tasks.py
@@ -29,7 +29,8 @@ def sims_in_orders():
         # Escolher operadora
         # if product_i == 'chip-internacional-europa' and countries_i == False and type_sim_i == 'esim':
         if product_i == 'chip-internacional-europa' and countries_i == False:
-            now = datetime.now()
+            # ALteração provisória
+            now = datetime.now().date()
             date_2_days = now + timedelta(days=2)
             if ord.activation_date > date_2_days:
                 operator_i = 'TC'
