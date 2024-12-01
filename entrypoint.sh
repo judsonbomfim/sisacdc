bf5248b65bf497d78dd32436b8d319b6c772bd66
Judson Bomfim <judsonbomfim@gmail.com>
2024-11-30 21:04:15 -0300

    Correção

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-nohup gunicorn core.wsgi:application --bind 0.0.0.0:8000 --log-level=info &
+nohup gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 120 --log-level=info &
 nohup celery -A core worker -B --loglevel=info
