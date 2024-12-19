85f427eb9beaedbac9a06e3d95a52f1b2c4cf960
Judson Bomfim <falecom@judsonbomfim.com>
2024-12-19 19:02:04 -0300

    Correção na importação dos planos Europa

--- apps/orders/tasks.py
+++ apps/orders/tasks.py
@@ -56,7 +56,7 @@ def order_import():
             for item in order['line_items']:
                                     
                 # Especificar produtos a serem listados
-                prod_sel = [50760, 8873, 8791, 8761, 77027, 79804]
+                prod_sel = [50760, 8873, 8761, 77027, 79804, 89373, 89526]
                 if item['product_id'] not in prod_sel:
                     continue
                                 
