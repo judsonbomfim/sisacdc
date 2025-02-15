dd2bf038015fec7c8c0b8164e37c79ef002726c6
Judson Bomfim <falecom@judsonbomfim.com>
2025-02-15 00:24:13 -0300

    Correções

--- apps/sims/classes.py
+++ apps/sims/classes.py
@@ -16,7 +16,7 @@ class ApiTC:
         headers_token = {
             'Content-Type': 'application/json',
             'X-Requested-With': 'XMLHttpRequest',
-            'X-Rate-Limit-TPS': '2',
+            'X-Rate-Limit-TPS': '200',
         }
         conn = http.client.HTTPSConnection(settings.APITC_HTTPCONN)
         conn.request("POST", "/api/login", payload_token, headers_token)
@@ -33,7 +33,7 @@ class ApiTC:
         headers = {
             'Content-Type': 'application/json',
             'X-Requested-With': 'XMLHttpRequest',
-            'X-Rate-Limit-TPS': '2',
+            'X-Rate-Limit-TPS': '200',
             'X-Authorization': f'Bearer {token_api}'
         }
         if cookie is None:
