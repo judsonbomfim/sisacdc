64f30ec1e250d4f641dc504014d889491e662ddb
Judson Bomfim <falecom@judsonbomfim.com>
2025-02-18 14:08:27 -0300

    Correções

--- apps/sims/tasks.py
+++ apps/sims/tasks.py
@@ -135,6 +135,11 @@ def simActivateTC(id=None):
         error = 'error_apiResult'
         return error
     
+    token_api = ApiTC.get_token()
+    conn = http.client.HTTPSConnection(settings.APITC_HTTPCONN)
+    print('>>>>>>>>>> token_api',token_api)
+    headers = ApiTC.get_headers(token_api)
+    
     for order in orders_all:
         
         time.sleep(1)
@@ -159,10 +164,6 @@ def simActivateTC(id=None):
         
         # Verificar EndPointID / Status
         try:
-            token_api = ApiTC.get_token()
-            conn = http.client.HTTPSConnection(settings.APITC_HTTPCONN)
-            print('>>>>>>>>>> token_api',token_api)
-            headers = ApiTC.get_headers(token_api)
             get_iccid = ApiTC.get_iccid(iccid, headers)
             endpointId = get_iccid[0]
             simStatus = get_iccid[1]
