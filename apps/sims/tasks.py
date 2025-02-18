43fadd921a5f26ce9bd2a2e4d9a20e424c3ea2cd
Judson Bomfim <falecom@judsonbomfim.com>
2025-02-18 12:52:23 -0300

    Correções TC

--- apps/sims/tasks.py
+++ apps/sims/tasks.py
@@ -133,9 +133,19 @@ def simActivateTC(id=None):
         # Adicionar nota
         NotesAdd.addNote(order,f'{iccid} com erro na Telcon. Verificar erro.')
         error = 'error_apiResult'
-        return error     
+        return error
+    
+    
+    try:
+        token_api = ApiTC.get_token()
+        conn = http.client.HTTPSConnection(settings.APITC_HTTPCONN)
+        print('>>>>>>>>>> token_api',token_api)
+    except Exception:            
+        error_api()
     
     for order in orders_all:
+        
+        time.sleep(0.5)
         
         order = Orders.objects.get(pk=order.id)
         order_id = order.order_id
@@ -157,9 +167,6 @@ def simActivateTC(id=None):
         
         # Verificar EndPointID / Status
         try:
-            token_api = ApiTC.get_token()
-            conn = http.client.HTTPSConnection(settings.APITC_HTTPCONN)
-            print('>>>>>>>>>> token_api',token_api)
             headers = ApiTC.get_headers(token_api)
             get_iccid = ApiTC.get_iccid(iccid, headers)
             endpointId = get_iccid[0]
