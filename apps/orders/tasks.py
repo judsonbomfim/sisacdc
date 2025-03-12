182f0ed7708ba466692eeb516966a75701835226
Judson Bomfim <falecom@judsonbomfim.com>
2025-03-12 18:04:25 -0300

    Correções

--- apps/orders/tasks.py
+++ apps/orders/tasks.py
@@ -601,6 +601,6 @@ def update_st():
                     print(f'XXXXXXXXXX = Erro ao atualizar status do pedido {id_order}')
                     continue
                 total_ord += 1
-                print(f'>>>>>>>>>> Pedido {id_order} > TOTAL = {total_ord}')
+                print(f'>>>>>>>>>> Pedido {id_ord} > TOTAL = {total_ord}')
 
         n_page += 1
