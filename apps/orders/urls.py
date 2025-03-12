4bc9719a70d3501f8d4b2b1f1f8dd469306109b9
Judson Bomfim <falecom@judsonbomfim.com>
2025-03-12 13:45:55 -0300

    Correção

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('enviar/esims/', views.send_esims, name='send_esims'),
     path('ativacoes/', views.orders_activations, name='orders_activations'),
     path('ativacoes/exportar', views.ord_export_act, name='ord_export_act'),
-    path('atualizar_status', views.atualizar_status, name='atualizar_status'),
+    path('atualizar_status', views.update_st, name='update_st'),
     
     # path('texto/', views.textImg, name='text_img'),
     # path('esimstore/', views.esimExpSis, name='esimstore'),
