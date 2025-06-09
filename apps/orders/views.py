f886beee959dd57a91728db68dce475cdc182cba
Judson Bomfim <falecom@judsonbomfim.com>
2025-06-09 14:09:18 -0300

    Correção

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -137,8 +137,8 @@ def ord_details(request, order_id):
     else:
         mobile_data = ''
     
-    if mobile_data != '':
-        mobile_data_f = f"{float(mobile_data):.2f}"
+    # if mobile_data != '':
+    #     mobile_data_f = f"{float(mobile_data):.2f}"
         
     print(f'Consumo de dados: {mobile_data} MB')
     
