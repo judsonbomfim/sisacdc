4a3eb0488740b9848c9f488b55f11d4114ae84ab
Judson Bomfim <falecom@judsonbomfim.com>
2025-06-10 16:16:11 -0300

    Correções

--- apps/sims/classes.py
+++ apps/sims/classes.py
@@ -216,9 +216,13 @@ class apiCM:
         # Corpo da requisição
         payload = json.dumps({
             "accessToken": api_token,
+            "himsi":"",        
             "iccid": iccid,
             "beginTime": dateStart,
             "endTime": dateStart,
+            "childOrderId":"",
+            "thirdOrderId":""
+            "ext": "",
         })
         
         # Fazer a requisição POST com tempo limite
