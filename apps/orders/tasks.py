d71a9cbb6b8835271286fd11f86123fc6b93351b
Judson Bomfim <falecom@judsonbomfim.com>
2025-07-11 18:20:29 -0300

    Correção

--- apps/orders/tasks.py
+++ apps/orders/tasks.py
@@ -275,6 +275,8 @@ def order_import_voice():
     while n_page <= total_pages:
         # Pedidos com status 'processing'
         response = apiStore.get('orders', params={'order': 'asc', 'status': 'processing', 'per_page': per_page, 'page': n_page})
+        print(response.status_code)
+        print(response.text)
         try:
             ord = response.json()
         except Exception as e:
