c6eb7dc8871f07678e77176c613dedec51267f35
Judson Bomfim <falecom@judsonbomfim.com>
2025-08-21 01:51:45 -0300

    Correção

--- apps/sims/classes.py
+++ apps/sims/classes.py
@@ -407,7 +407,7 @@ class apiCM:
         payload = json.dumps({
             "accessToken": api_token,
             "iccid": iccid,
-            "childOrderId": "1656854506349832930",
+            "childOrderId": "1956854506349832930",
             "beginTime": "20250819",
             "endTime": "20250819",
             
