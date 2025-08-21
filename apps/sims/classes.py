b6a3e521524b4fe35966845f90a993500aeca920
Judson Bomfim <falecom@judsonbomfim.com>
2025-08-21 02:07:17 -0300

    Teste

--- apps/sims/classes.py
+++ apps/sims/classes.py
@@ -408,8 +408,8 @@ class apiCM:
             "accessToken": api_token,
             "iccid": iccid,
             "childOrderId": "1956854506349832930",
-            "beginTime": "20250820",
-            "endTime": "20250820",            
+            "beginTime": "20250819",
+            "endTime": "20250819",
         })
 
         # Fazer a requisição POST com tempo limite
