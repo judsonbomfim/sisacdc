b9b0f6b46731388deda2a7a2954c2fbce98a3a26
Judson Bomfim <falecom@judsonbomfim.com>
2025-08-26 18:52:20 -0300

    Correção na consulta do SIM

--- apps/sims/views/api.py
+++ apps/sims/views/api.py
@@ -10,19 +10,16 @@ class ConsumoView(APIView):
     permission_classes = [IsAuthenticated]  # Requer autenticação JWT
 
     def get(self, request, iccid):
-
-        sim = Sims.objects.filter(id_sim__sim=iccid).first()
+        sim = Sims.objects.filter(sim=iccid).first()
         sim_operator = sim.operator if sim else None
         print(f"ICCID recebido: {iccid}")
         print(f"Operadora do SIM: {sim_operator}")
 
         try:
-            # Chama o método mobileData da classe ApiTC
             if sim_operator == 'TC':
                 mobile_data = ApiTC.mobileData(iccid)
             elif sim_operator == 'CM':
                 mobile_data = ApiCM.mobileData(iccid)
-            # Serializa os dados
             serializer = ConsumoSerializer(data={
                 "iccid": iccid,
                 "mobile_data": mobile_data
@@ -31,5 +28,4 @@ class ConsumoView(APIView):
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            # Tratamento genérico de erros
             return Response({"error": f"Erro ao consultar consumo: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
