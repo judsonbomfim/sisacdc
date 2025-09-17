49eb42084b392735931126054f2d7f4b479aa345
Judson Bomfim <falecom@judsonbomfim.com>
2025-09-17 15:53:12 -0300

    Correções

--- apps/voice_calls/views.py
+++ apps/voice_calls/views.py
@@ -6,7 +6,8 @@ from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.conf import settings
-from apps.voice_calls.models import NotesVoice, VoiceNumbers, VoiceCalls
+from apps.voice_calls.classes import NoteVoiceCall
+from apps.voice_calls.models import VoiceNumbers, VoiceCalls
 from apps.voice_calls.tasks import number_up_status, voices_up_status, update_password
 from apps.orders.models import Orders
 from apps.orders.classes import DateFormats
@@ -150,7 +151,8 @@ def voice_edit(request,id):
     if request.method == 'POST':
                 
         call_put = VoiceCalls.objects.get(pk=id)
-        call_put.days = request.POST.get('days')
+        if request.POST.get('days'):
+            call_put.days = request.POST.get('days')
         if request.POST.get('activation_date'):
             call_put.activation_date = request.POST.get('activation_date')
         else:
@@ -159,7 +161,7 @@ def voice_edit(request,id):
         call_put.save()
         
         if request.POST.get('ord_note'):
-            NotesVoice.addNote(id_item=id, note=request.POST.get('ord_note'), id_user=request.user.id, type_note='P')
+            NoteVoiceCall.addNote(id_item=id, note=request.POST.get('ord_note'), id_user=request.user.id, type_note='P')
         
         messages.success(request,f'Pedido {call_put.id_item} atualizado com sucesso!')
         return redirect('voice_index')
