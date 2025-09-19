022f6bd56fa787a9feaf928a8e85fbe4299efcce
Judson Bomfim <falecom@judsonbomfim.com>
2025-09-19 12:08:54 -0300

    Correção

--- apps/orders/classes.py
+++ apps/orders/classes.py
@@ -47,6 +47,7 @@ class UpdateStore():
     @staticmethod
     def upStore(order_id, item_id_store=None, _data_ativacao=None, _sim=None, _qrcode=None, _status=None,status_g=None):
         meta_data = []
+        update_store = {}
         print(f'---------- Atualizando pedido {order_id} na loja.')
         print(f'---------- status_g: {status_g}')
         if item_id_store != None:
