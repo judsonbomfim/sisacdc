1825f5e91a5b7e4f582a294c3e934d30c8fad3ba
Judson Bomfim <falecom@judsonbomfim.com>
2025-10-02 21:12:31 -0300

    Correção da API MS

--- apps/sims/tasks.py
+++ apps/sims/tasks.py
@@ -1265,10 +1265,11 @@ def simActivateMS(id=None):
             response_data = response.json()
             logger.info(f"Resposta da API para o pedido {order.order_id}: {response_data}")
 
-            if 'hash' in response_data:
+            if response_data[0]['hash']:
                 order.get_sim = response_data['hash']
                 order.save()
                 logger.info(f'Pedido {order.order_id} ativado com sucesso na MS. Hash: {order.get_sim}')
+                UpdateOrder.upStatus(order.id, 'AT')
                 UpdateStore.upStore(
                     order_id = order.order_id,
                     item_id_store = order.item_id_store if order.item_id_store else None,
