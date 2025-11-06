fb2ef4faa4f91ef7a14f757d18d3d1622116a1b8
Judson Bomfim <falecom@judsonbomfim.com>
2025-11-06 20:06:53 -0300

    Correções

--- apps/send_email/tasks.py
+++ apps/send_email/tasks.py
@@ -92,11 +92,11 @@ def send_email_sims(id=None):
                 try:
                     UpdateStore.upStore(
                         order_id = order_id,
-                        item_id_store = order.item_id_store,
-                        _data_ativacao=str(activation_date) if activation_date else None,
+                        item_id_store = order.item_id_store if order.item_id_store else None,
+                        _data_ativacao = str(activation_date) if activation_date else None,
                         _sim = sim if sim else None,
                         _qrcode = qrcode if qrcode else None,
-                        _status='AA',
+                        _status ='AA',
                         status_g = 'AA',
                     )
                     print(f">>>>>>>>>> Status AA atualizado no site - Pedido: {order_id}")
