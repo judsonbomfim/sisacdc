e12c3e1167cc846c1ae9db26b535a7b4221926a0
Judson Bomfim <falecom@judsonbomfim.com>
2025-11-06 18:29:56 -0300

    Correções

--- apps/orders/classes.py
+++ apps/orders/classes.py
@@ -80,9 +80,11 @@ class UpdateStore():
                         "meta_data": meta_data
                     }
                 ]}
-        if status_g:
+        if status_g or _status:
+            print(f">>>>>>>>>> Status {_status} / {status_g}")
             status_sis_site = StatusStore.st_sis_site()
             print(f">>>>>>>>>> Atualizando status geral para {status_sis_site[status_g]} no site - Pedido: {order_id}")
+            update_store['status'] = status_sis_site[_status]
             update_store['status'] = status_sis_site[status_g]
         if update_store:
             apiStore = ApiStore.conectApiStore()
