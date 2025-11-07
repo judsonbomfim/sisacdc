d5adcb0280bfa8a3c9308f1267569303b09500ff
Judson Bomfim <falecom@judsonbomfim.com>
2025-11-07 20:02:31 -0300

    AJustes

--- core/settings.py
+++ core/settings.py
@@ -286,11 +286,11 @@ LOGGING = {
             'class': 'logging.StreamHandler',
             'formatter': 'verbose',
         },
-        'file': {
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),
-            'formatter': 'verbose',
-        },
+        # 'file': {
+        #     'class': 'logging.FileHandler',
+        #     'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),
+        #     'formatter': 'verbose',
+        # },
     },
     'root': {
         'handlers': ['console', 'file'],
