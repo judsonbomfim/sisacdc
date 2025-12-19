f07edc1a6230924a8d53e81147fcc020afe00556
Judson Bomfim <falecom@judsonbomfim.com>
2025-12-19 15:45:41 -0300

    Correção de erros

--- apps/voice_calls/tasks.py
+++ apps/voice_calls/tasks.py
@@ -286,14 +286,3 @@ def voiceDesactivate(id=None):
         conn.close()
                 
     print('>>>>>>>>>> ATIVAÇÂO VOICE FINALIZADA')
-
-
-# Aliases to match Celery beat names configured in settings
-@shared_task
-def simActivateVC(id=None):
-    return voiceActivate(id=id)
-
-
-@shared_task
-def simDeactivateVC(id=None):
-    return voiceDesactivate(id=id)
