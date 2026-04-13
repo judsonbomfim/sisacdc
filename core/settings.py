aba30a789c44a205bdf6a6be5fe3df2f7bef823e
Judson Bomfim <falecom@judsonbomfim.com>
2026-04-13 19:42:02 -0300

    Task AT

--- core/settings.py
+++ core/settings.py
@@ -346,6 +346,10 @@ CELERY_BEAT_SCHEDULE = {
         'task': 'apps.sims.tasks.simActivateOR',
         'schedule': crontab(minute='4-59/2'),
     },
+    'task__2_min_simActivateAT': {
+        'task': 'apps.sims.tasks.simActivateAT',
+        'schedule': crontab(minute='2-59/2'),
+    },
     'task__2_min_simAgdOperator': {
         'task': 'apps.sims.tasks.simAgdOperator',
         'schedule': crontab(minute='2-59/2'),
