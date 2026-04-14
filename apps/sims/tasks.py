6617681e2f5953f6a9afcab80318d411d8517a4b
Judson Bomfim <falecom@judsonbomfim.com>
2026-04-13 22:06:06 -0300

    Logs

--- apps/sims/tasks.py
+++ apps/sims/tasks.py
@@ -1715,7 +1715,7 @@ def simAgdOperator():
             response = requests.post(url, headers=headers, params=payload) 
             response_data = response.json()
             logger.info(f'Pedido {order.order_id} - Resposta da AT&T: {response_data}')
-            status_now = response_data['status']['name']
+            status_now = response_data['status']['name'] if 'status' in response_data and 'name' in response_data['status'] else 'Status desconhecido'
             logger.info(f'Pedido {order.order_id} - Status atual na AT&T: {status_now}')
         except requests.exceptions.HTTPError as e:
             logger.error(f"Erro HTTP ao consultar o status do pedido {order.order_id} na AT&T: {e}")
@@ -1728,7 +1728,6 @@ def simAgdOperator():
             continue
         
         if status_now == 'completed':
-            
             sim_value = response_data['products'][0]['sim_data']['iccid']
             lpa_value = response_data['products'][0]['sim_data']['lpa_code']
             
