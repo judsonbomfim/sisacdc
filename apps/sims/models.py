0f8f173a5c7a07f3de42b2bee1c56c54908c7a04
Judson Bomfim <falecom@judsonbomfim.com>
2026-06-24 09:05:23 -0300

    Correção ativação OR

--- apps/sims/models.py
+++ apps/sims/models.py
@@ -56,6 +56,7 @@ class Sims(models.Model):
     """
     id = models.AutoField(primary_key=True, serialize=False)
     sim = models.CharField(max_length=25)
+    msisdn = models.IntegerField(null=True, blank=True)
     lpa = models.CharField(max_length=255, null=True, blank=True, )
     link = models.URLField(null=True, blank=True, default='-')
     type_sim =  models.CharField(max_length=20, choices=SIM_TYPES)
